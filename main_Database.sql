312dcd9738be2554554e35a34ec90a5226807801
aquinones154 <113639433+aquinones154@users.noreply.github.com>
2024-09-25 20:54:19 -0500

    added Match, Stadium, World_champions,team, group,

--- main_Database.sql
+++ main_Database.sql
@@ -6,5 +6,44 @@ CREATE TABLE Player(
     Pname VARCHAR(15),
     Position VARCHAR(15),
     DOB DATE,
-    PRIMARY KEY (PID);
-);
+    PRIMARY KEY (PID),
+    FOREGIN KEY (TID) REFERENCES Team(TID);
+)
+
+CREATE TABLE Match (
+    MID INT, 
+    MData Date, 
+    Winner VARCHAR(15),
+    PRIMARY KEY (MID);
+    FOREGIN KEY REFERENCES Stadium(SID);
+)
+
+CREATE TABLE Stadium(
+    SID INT,
+    Sname VARCHAR(15),
+    Capacity INT,
+    City VARCHAR(15),
+    PRIMARY KEY(SID);
+)
+
+CREATE TABLE World_Champions (
+    WID INT, 
+    Year INT,
+    PRIMARY KEY(WID);
+)
+
+CREATE TABLE Team (
+    TID INT,
+    Country VARCHAR(15),
+    Coach VARCHAR(15),
+    PRIMARY KEY (TID);
+)
+
+CREATE TABLE Group (
+    GID INT,
+    Gname VARCHAR(15),
+    Teams VARCHAR(15),
+    Matches VARCHAR(15), 
+    PRIMARY KEY (GID); 
+)
+
