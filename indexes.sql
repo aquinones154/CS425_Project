d48b26b24a7fe21de1583b96a26c95afe9f67967
aquinones154 <113639433+aquinones154@users.noreply.github.com>
2024-09-27 16:58:41 -0500

    created indexes file and added sample indexes + one compund index

--- indexes.sql
+++ indexes.sql
@@ -1,1 +1,16 @@
+-- file has Indexes
 
+-- index for Player's table to look up name faster
+CREATE INDEX idx_player_pname ON Player(Pname);
+
+-- index for Match table to look up winner faster
+CREATE INDEX idx_match_winner ON Match(Winner);
+
+-- index for Goals table to look up time of goal faster
+CREATE INDEX idx_goals_time_of_goal ON Goals(Time_of_Goal);
+
+-- index for goals table to look up group name
+CREATE INDEX idx_group_gname ON `Group`(Gname);
+
+-- compound index for team_match table on MID and TID
+CREATE INDEX idx_team_match_mid_tid ON team_match(MID, TID);
