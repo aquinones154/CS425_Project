ecbc82ff14e0e1ba078f461787bd8565b14ea075
aquinones154 <113639433+aquinones154@users.noreply.github.com>
2024-10-02 13:21:28 -0500

    added another test query

--- test_queries.sql
+++ test_queries.sql
@@ -6,19 +6,18 @@
 SELECT * 
 FROM Player;
 
-
--- gets everyting from goals table
-SELECT * 
-FROM Goals; 
-
 -- gets capactiy from stadium table and sorts it by ascending order
 SELECT Capacity, Sname
 FROM Stadium
 ORDER BY Capacity ASC; 
 
 -- Inserts data into gameMatch table 
-
 INSERT INTO GameMatch (MID,MData,Winner,Home_team_score,Away_team_score) VALUES (46,2004-10-09,'Team1', 3, 2, 15);
+
+-- Inserts data into TeamGroup table
 INSERT INTO TeamGroup (GID, Gname, Teams, Matches) VALUES (24, 'NorthEast', 'TeamsA-D', 'test');
 
-
+-- outputs the goals scored after halftime  
+SELECT Time_of_Goal
+FROM Goals
+WHERE Time_of_Goal > 45; 
