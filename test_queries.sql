b09e92eedd7c5bf043f906e24a91628e6ce1958d
aquinones154 <113639433+aquinones154@users.noreply.github.com>
2024-10-02 10:03:35 -0500

    added some simple queries

--- test_queries.sql
+++ test_queries.sql
@@ -1,1 +1,19 @@
 -- file for test queries for deliverable #3
+
+-- simple queries
+
+-- gets everyting from player table
+SELECT * 
+FROM Player;
+
+
+-- gets everyting from goals table
+SELECT * 
+FROM Goals; 
+
+-- gets capactiy from stadium table and sorts it by ascending order
+
+SELECT Capacity, Sname
+FROM Stadium
+ORDER BY Capacity ASC; 
+
