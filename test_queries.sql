6055912f525786b0d7182563c8f6927bfd2384d2
aquinones154 <113639433+aquinones154@users.noreply.github.com>
2024-10-08 13:29:43 -0500

    ordered queries by type

--- test_queries.sql
+++ test_queries.sql
@@ -1,6 +1,6 @@
 -- file for test queries for deliverable #3
 
--- simple queries
+-- select query
 
 -- gets everyting from player table
 SELECT * 
@@ -11,13 +11,19 @@ SELECT Capacity, Sname
 FROM Stadium
 ORDER BY Capacity ASC; 
 
+-- outputs the goals scored after halftime  
+SELECT Time_of_Goal
+FROM Goals
+WHERE Time_of_Goal > 45; 
+
+-- insert query
+
 -- Inserts data into gameMatch table 
 INSERT INTO GameMatch (MID,MData,Winner,Home_team_score,Away_team_score) VALUES (46,2004-10-09,'Team1', 3, 2, 15);
 
 -- Inserts data into TeamGroup table
 INSERT INTO TeamGroup (GID, Gname, Teams, Matches) VALUES (24, 'NorthEast', 'TeamsA-D', 'test');
 
--- outputs the goals scored after halftime  
-SELECT Time_of_Goal
-FROM Goals
-WHERE Time_of_Goal > 45; 
+-- include advance window features
+-- olap query
+-- 
