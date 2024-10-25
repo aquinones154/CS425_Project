fe7c4c12f04c83e7846a6240e9fd55429381a455
aquinones154 <113639433+aquinones154@users.noreply.github.com>
2024-10-24 19:25:31 -0500

    added selection choice for user

--- pythonscript.py
+++ pythonscript.py
@@ -1,8 +1,8 @@
 import mysql.connector
 
-host="localhost",  
-user="root",  # Database username
-password="password",  # Database password
+host="localhost" # host name
+user="root"  # Database username
+password="Boba2021"  # Database password
 database="womensWrlCUP"  # Name of the database to use
 
 
@@ -19,6 +19,31 @@ try:
         print("Connected to MYSQL database successfully")
 
         cursor = connection.cursor()
-    
+
+        #print statemnts for user to select what they want to do
+        userChoice= input('1. Read Data \n 2. Write Data \n 3. Insert Data \n 4. Update Data \n 5. Exit \n Enter which on you would like to do:  ')
+        
+
+        if(userChoice == '1' ):
+            #read data
+            print("read data")
+        elif(userChoice == '2'):
+            #write data
+            print("write data")
+        elif(userChoice == '3'):
+            #Insert data
+            print("insert data")
+        elif(userChoice == '4'):
+             #Update data
+            print("update data")
+        elif(userChoice == '5'):
+         #exit
+            print("exit")
+
+        else:
+            print("NOT A VALID CHOICE")
+
+        
+
 except:
     print("ERROR: COULD NOT CONNECT TO MYSQL DATABASE")
