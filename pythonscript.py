f6b892887c67418d60b13d05ad015c73b60c5cd7
aquinones154 <113639433+aquinones154@users.noreply.github.com>
2024-10-25 08:29:36 -0500

    updated read function so it can read from different tables

--- pythonscript.py
+++ pythonscript.py
@@ -22,29 +22,48 @@ try:
         cursor = connection.cursor()
 
         #print statemnts for user to select what they want to do
-        userChoice= input('1. Read Data \n 2. Create Data \n 3. Delete Data \n 4. Update Data \n 5. Exit \n Enter which on you would like to do:  ')
+        userChoice= input(' 1. Read Data \n 2. Create Data \n 3. Delete Data \n 4. Update Data \n 5. Exit \n Enter which on you would like to do:  ')
         
+        #function to read data
+        def read_data(table_name):
+            try:
+                select_query = f"SELECT * FROM {table_name}" #default query is select all from a table
+                cursor.execute(select_query)
+                return cursor.fetchall()
+            except Exception as e: #exception handling
+                print(f"ERROR COULD NOT READ DATA FROM: {table_name}: {e}") #if there is an expcetion, then tell user the table name and what the exception is
+                return None #end
 
-        #READ DATA
-        if (userChoice == '1'):
-            def read_data():
-                try:
-                # Read data from table
-                    select_query = "SELECT * FROM Player"  # selects everything from player table
-                    cursor.execute(select_query)
-                    player_data = cursor.fetchall()  # Fe
-                    return player_data
-                except Exception as e:
-                    print(f"ERROR COULD NOT READ DATA {e}")
-                    return None
-
-            player_data = read_data()
-            if player_data:
-                print(player_data)  #prints data
-                print("\n READ DATA COMPLETE \n") #print message to show it worked
+        # READ DATA
+        if userChoice == '1': 
+            userChoice_read = input('Choose which table to read from: \n 1. Team \n 2. Stadium \n 3. Player \n 4. GameMatch \n 5. Goals \n 6. Team_match \n 7. World_Champions \n 8. TeamGroup \n 9. Team_tournament \n Please choose one: ') #asking user to pick a table to read from
+            
+            #map a number to a table name
+            table_mapping = {
+                '1': 'Team',
+                '2': 'Stadium',
+                '3': 'Player',
+                '4': 'GameMatch',
+                '5': 'Goals',
+                '6': 'Team_match',
+                '7': 'World_Champions',
+                '8': 'TeamGroup',
+                '9': 'Team_tournament'
+            }
+            
+            if userChoice_read in table_mapping: #if a valid choice then read data
+                table_name = table_mapping[userChoice_read]
+                data = read_data(table_name)
+                if data:
+                    print(f"\n Data from {table_name}: \n") #printing data
+                    for row in data:
+                        print(row)
+                    print("\n READ DATA COMPLETE \n") #print for sucessful completion of data read
+                else:
+                    print("ERROR COULD NOT READ DATA") #print if data could not be read
             else:
-                print("ERROR COULD NOT READ DATA") #if it didnt work then print error message
-
+                print("INVALID CHOICE") #print message only if choice is not valid
+        
 
         #WRITE DATA
         elif(userChoice == '2'):
