3b41cc38700d3233fa1c67b091ebd458e2149325
aquinones154 <113639433+aquinones154@users.noreply.github.com>
2024-10-25 14:11:32 -0500

    added more options whne creating data

--- pythonscript.py
+++ pythonscript.py
@@ -67,26 +67,70 @@ try:
 
         #CREATE DATA
         elif(userChoice == '2'):
+            
             def create_data():
-                try: 
-                    userChoice_Create = input('Choose which table to Create data in: \n 1. Team \n 2. Stadium \n 3. Player \n 4. GameMatch \n 5. Goals \n 6. Team_match \n 7. World_Champions \n 8. TeamGroup \n 9. Team_tournament \n Please choose one: ') #asking user to pick a table to create data in 
+                try:
+                    # Ask the user which table they want to insert data into
+                    userChoice_create = input(
+                        'Choose which table to create data in: \n' #pick from the following 
+                        '1. Team \n'
+                        '2. Stadium \n'
+                        '3. Player \n'
+                        'Please choose one: '
+                    )
 
-                    #map a number to a table name
+                    # map user choice to a table
                     table_mapping = {
-                    '1': 'Team',
-                    '2': 'Stadium',
-                    '3': 'Player',
-                    '4': 'GameMatch',
-                    '5': 'Goals',
-                    '6': 'Team_match',
-                    '7': 'World_Champions',
-                    '8': 'TeamGroup',
-                    '9': 'Team_tournament'
+                        '1': 'Team',
+                        '2': 'Stadium',
+                        '3': 'Player',
                     }
-                  
+
+                    # get table based off of user choice
+                    table_name = table_mapping.get(userChoice_create)
+
+                    if not table_name:
+                        print("Invalid choice") #making sure chocie is valid
+                        return
+                    
+                    # if team table then get the new data to be created
+                    if table_name == 'Team':
+                        tid = random.randint(1, 1000) #creating random TID for team
+                        country = input("Enter team country: ") #asking user for country name
+                        coach = input("Enter coach name: ") #asking user for coach name
+                        insert_query = "INSERT INTO Team (TID, Country, Coach) VALUES (%s, %s, %s)" #query to insert data into team table
+                        data = (tid, country, coach) #save it into data to be used at end of function
+
+                    #if Stadium table then get the new data to be created
+                    elif table_name == 'Stadium':
+                        sid = random.randint(1, 1000) #creating random SID 
+                        sname = input("Enter stadium name: ") #ask user for stadium name
+                        capacity = input("Enter capacity: ") #ask user for capacity
+                        city = input("Enter city: ") #ask user for city 
+                        insert_query = "INSERT INTO Stadium (SID, Sname, Capacity, City) VALUES (%s, %s, %s, %s)" #query to inser data into stadium table
+                        data = (sid, sname, capacity, city) #save it into data to be used at the end of function
+
+
+                    elif table_name == 'Player': #if player table then get new data to be created
+                        pid = random.randint(1, 1000) #create new random PID
+                        pname = input("Enter player name: ") # enter name for new player
+                        position = input("Enter player position: ") #enter postiion for player
+                        dob = input("Enter date of birth (YYYY-MM-DD): ") #etner date of birth for new player
+                        age = input("Enter player age: ") #enter age for player
+                        tid = input("Enter team ID: ") #enter team ID for player
+                        insert_query = "INSERT INTO Player (PID, Pname, Position, DOB, Age, TID) VALUES (%s, %s, %s, %s, %s, %s)"
+                        data = (pid, pname, position, dob, age, tid)
+
+                    # Execute the insert query
+                    cursor.execute(insert_query, data)
+                    connection.commit()
+
+                    print(f"\nData inserted successfully into {table_name}.\n")
+                
                 except Exception as e:
                     print(f"ERROR COULD NOT CREATE DATA: {e}")
 
+            create_data()
 
         elif(userChoice == '3'):
             #Delete data
